e77224ef1b31ee6366ef2ad314888db4ad210d41
Modesto Tamayo <modesto.t.tamayo@gmail.com>
2019-05-29 15:40:30 -0500

    solved the string_dup function

--- malloc/malloc.c
+++ malloc/malloc.c
@@ -12,7 +12,10 @@
 */
 char *string_dup(char *src)
 {
+    char *dup = malloc(string_length(src));
+    dup = src;
 
+    return dup; 
 }
 
 /*
@@ -51,36 +54,36 @@ int main(void)
 
     printf("Duplicated string: %s\n", dup);
 
-    int numbers[] = {100, 55, 4, 98, 10, 18, 90, 95, 43, 11, 47, 67, 89, 42, 49, 79};
-    int n = sizeof(numbers) / sizeof(numbers[0]);
-    int *target = malloc(n * sizeof(int));
+    // int numbers[] = {100, 55, 4, 98, 10, 18, 90, 95, 43, 11, 47, 67, 89, 42, 49, 79};
+    // int n = sizeof(numbers) / sizeof(numbers[0]);
+    // int *target = malloc(n * sizeof(int));
     
-    mem_copy(target, numbers, n * sizeof(int));
+    // mem_copy(target, numbers, n * sizeof(int));
 
-    printf("Copied array: ");
+    // printf("Copied array: ");
 
-    for (int i = 0; i < n; i++) {
-        printf("%d ", target[i]);
-    }
+    // for (int i = 0; i < n; i++) {
+    //     printf("%d ", target[i]);
+    // }
 
-    printf("\n");
+    // printf("\n");
 
-    char *url = string_dup("http://lambdaschool.com");
-    char *path = string_dup("/students/");
-    int url_length = string_length(url);
-    int path_length = string_length(path);
+    // char *url = string_dup("http://lambdaschool.com");
+    // char *path = string_dup("/students/");
+    // int url_length = string_length(url);
+    // int path_length = string_length(path);
     
-    int new_length = url_length - 1 + path_length;
-    char *new_url = resize_memory(url, url_length, new_length);
-    char *p = new_url + url_length;
+    // int new_length = url_length - 1 + path_length;
+    // char *new_url = resize_memory(url, url_length, new_length);
+    // char *p = new_url + url_length;
 
-    while (*path != '\0') {
-        *p = *path;
-        p++;
-        path++;
-    }
+    // while (*path != '\0') {
+    //     *p = *path;
+    //     p++;
+    //     path++;
+    // }
 
-    printf("Full path string: %s\n", new_url);
+    // printf("Full path string: %s\n", new_url);
 
     return 0;
 }
